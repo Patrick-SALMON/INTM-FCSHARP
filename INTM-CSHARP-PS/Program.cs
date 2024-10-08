156af24ec682992c0ed672ae403ab91dda7bcb10
Patrick-SALMON <salmon.patrick.pro@gmail.com>
2024-10-08 16:59:11 +0200

    Série 2 commencée

--- INTM-CSHARP-PS/Program.cs
+++ INTM-CSHARP-PS/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using INTM.Serie1;
+using INTM.Serie2;
 
 namespace INTM_CSHARP_PS
 {
@@ -11,7 +12,12 @@ namespace INTM_CSHARP_PS
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("|              Série 1                 |");
+            Console.WriteLine("|--------------------------------------|\n");
+
+            Console.WriteLine("\n|--------------------------------------|");
             Console.WriteLine("| Exercice I - Opérations élémentaires |");
             Console.WriteLine("|--------------------------------------|\n");
 
@@ -79,6 +85,68 @@ namespace INTM_CSHARP_PS
             } while (!int.TryParse(input, out b));
 
             Console.WriteLine($"\nPGCD de {a} et {b} : {Serie1.Gcd(a, b)}");
+            */
+
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("|              Série 2                 |");
+            Console.WriteLine("|--------------------------------------|\n");
+
+            Console.WriteLine("\n|--------------------------------------|");
+            Console.WriteLine("| Exercice I - Recherche d'un élément  |");
+            Console.WriteLine("|--------------------------------------|\n");
+
+            int[] tab = { 1, -5, 10, 3, 0, 4, 2, -7 };
+            int[] tabtrie = { -7, -5, 0, 1, 2, 3, 4, 10 };
+
+            Console.WriteLine("Recherche linéaire: ");
+            Console.WriteLine($"L'élément 2 du tableau est à l'indice {Serie2.LinearSearch(tab, 2)}");
+            Console.WriteLine($"L'élément -8 du tableau est à l'indice {Serie2.LinearSearch(tab, -8)}");
+
+            Console.WriteLine("\nRecherche dichotomique: ");
+            Console.WriteLine($"L'élément 2 du tableau est à l'indice {Serie2.BinarySearch(tabtrie, 2)}");
+            Console.WriteLine($"L'élément -8 du tableau est à l'indice {Serie2.BinarySearch(tabtrie, -8)}");
+            
+            Console.WriteLine("\n|------------------------------------------|");
+            Console.WriteLine("| Exercice II - Bases du calcul matriciel  |");
+            Console.WriteLine("|------------------------------------------|\n");
+
+            int[] gauche = { 1, 2, 3 };
+            int[] droite = { -1, -4, 0 };
+            int[][] matrice = Serie2.BuildingMatrix(gauche, droite);
+
+            Console.WriteLine("Matrice construite à partir de deux vecteurs: ");
+            Serie2.DisplayMatrix(matrice);
+
+            int[][] mGaucheAdd = new int[3][] { new int[2] { 1, 2 }, new int[2] { 4, 6 }, new int[2] { -1, 8 } };
+            int[][] mDroiteAdd = new int[3][] { new int[2] { -1, 5 }, new int[2] { -4, 0 }, new int[2] { 0, 2 } };
+
+            matrice = Serie2.Addition(mGaucheAdd, mDroiteAdd);
+
+            Console.WriteLine("\nAddition de deux matrices: ");
+            Serie2.DisplayMatrix(matrice);
+
+            int[][] mGaucheMult = new int[3][] { new int[2] { 1, 2 }, new int[2] { 4, 6 }, new int[2] { -1, 8 } };
+            int[][] mDroiteMult = new int[2][] { new int[3] { -1, 5, 0 }, new int[3] { -4, 0, 1 } };
+
+            matrice = Serie2.Multiplication(mGaucheMult, mDroiteMult);
+
+            Console.WriteLine("\nMultiplication de deux matrices: ");
+            Serie2.DisplayMatrix(matrice);
+
+            Console.WriteLine("\n|------------------------------------------|");
+            Console.WriteLine("| Exercice III - Crible d'Erastothène      |");
+            Console.WriteLine("|------------------------------------------|\n");
+
+            int[] tabPremier = Serie2.EratosthenesSieve(100);
+
+            Console.WriteLine("100 premiers nombres premiers d'après le crible d'Eratosthène:\n ");
+
+            Console.Write("[ ");
+            for (int i = 0; i < tabPremier.Length; i++)
+            {
+                Console.Write(tabPremier[i] + " ");
+            }
+            Console.Write("]\n");
 
             // Keep the console window open
             Console.WriteLine("\n----------------------");
---
+++ INTM-CSHARP-PS/Série 2/Serie2.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTM.Serie2
+{
+    class Serie2
+    {
+        // Exercice I - Recherche d'un élément
+
+        // Dans le pire des cas, la méthode linéaire doit lire tous les éléments du tableau
+        public static int LinearSearch(int[] tableau, int valeur)
+        {
+            if (tableau.Length == 0)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < tableau.Length; i++) 
+            {
+                if (tableau[i] == valeur)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        // Dans le pire des cas, la méthode dichotomique doit lire environ log(N) éléments avec N la taille du tableau
+        public static int BinarySearch(int[] tableau, int valeur)
+        {
+            if (tableau.Length == 0) 
+            {
+                return -1;
+            }
+
+            int milieu;
+            int gauche = 0;
+            int droite = tableau.Length - 1;
+
+            while (gauche <= droite)
+            {
+                milieu = (gauche + droite) / 2;
+
+                if (valeur == tableau[milieu])
+                {
+                    return milieu;
+                }
+                else if (valeur > tableau[milieu] )
+                {
+                    // + 1 car on sait que milieu n'est pas la bonne valeur et pour la condtion d'arrêt
+                    gauche = milieu + 1;
+                }
+                else
+                {
+                    // - 1 car on sait que milieu n'est pas la bonne valeur et pour la condition d'arrêt
+                    droite = milieu - 1;
+                }
+            }
+            return -1;
+        }
+
+        // Exercice II - Bases du calcul matriciel
+
+        /// <summary>
+        /// Construit une matrice d'après deux vecteurs de la même taille en prenant la transposée du premier et en la multipliant par le deuxième.
+        /// </summary>
+        /// <param name="leftVector"> Vecteur de gauche</param>
+        /// <param name="rightVector"> Vecteur de droite</param>
+        /// <returns>Une matrice vide si les vecteurs ne sont pas de la même taille ou une matrice carrée si ils le sont.</returns>
+        public static int[][] BuildingMatrix(int[] leftVector, int[] rightVector)
+        {
+            if (leftVector.Length != rightVector.Length)
+            {
+                return new int[0][];
+            }
+
+            int mLineLength = leftVector.Length;
+            int mColLength = rightVector.Length;
+            int[][] mRes = new int[mLineLength][];
+
+            for (int i = 0; i < mLineLength; i++)
+            {
+                mRes[i] = new int[mColLength];
+                for (int j = 0; j < mColLength; j++)
+                {
+                    mRes[i][j] = leftVector[i] * rightVector[j];
+                }
+            }
+            return mRes;
+        }
+
+        /// <summary>
+        /// Affiche une matrice reçu en entrée
+        /// </summary>
+        /// <param name="matrix">Matrice à afficher.</param>
+        public static void DisplayMatrix(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.Write("( ");
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+                Console.Write(")\n");
+            }
+        }
+
+        /// <summary>
+        /// Réalise l'addition de deux matrices de même taille.
+        /// </summary>
+        /// <param name="leftMatrix">Matrice de gauche.</param>
+        /// <param name="rightMatrix">Matrice de droite.</param>
+        /// <returns>Une matrice résultat correspondant à l'addition des deux matrices d'entrée ou une matrice vide si les dimensions sont incorrectes.</returns>
+        public static int[][] Addition(int[][] leftMatrix, int[][] rightMatrix)
+        {
+            if (leftMatrix.Length != rightMatrix.Length || leftMatrix[0].Length != rightMatrix[0].Length)
+            {
+                return new int[0][];
+            }
+
+            int mLineLength = rightMatrix.Length;
+            int mColLength = rightMatrix[0].Length;
+            int[][] mRes = new int[mLineLength][];
+
+            for (int i = 0; i < mLineLength; i++)
+            {
+                mRes[i] = new int[mColLength];
+                for (int j = 0; j < mColLength; j++)
+                {
+                    mRes[i][j] = leftMatrix[i][j] + rightMatrix[i][j];
+                }
+            }
+
+            return mRes;
+        }
+
+        /// <summary>
+        /// Réalise la soustraction de deux matrices de même taille.
+        /// </summary>
+        /// <param name="leftMatrix">Matrice de gauche.</param>
+        /// <param name="rightMatrix">Matrice de droite.</param>
+        /// <returns>Une matrice résultat correspondant à la soustraction de la matrice de gauche moins celle de droite ou une matrice vide si les dimensions sont incorrectes.</returns>
+        public static int[][] Substraction(int[][] leftMatrix, int[][] rightMatrix)
+        {
+            if (leftMatrix.Length != rightMatrix.Length || leftMatrix[0].Length != rightMatrix[0].Length)
+            {
+                return new int[0][];
+            }
+
+            int mLineLength = rightMatrix.Length;
+            int mColLength = rightMatrix[0].Length;
+            int[][] mRes = new int[mLineLength][];
+
+            for (int i = 0; i < mLineLength; i++)
+            {
+                mRes[i] = new int[mColLength];
+                for (int j = 0; j < mColLength; j++)
+                {
+                    mRes[i][j] = leftMatrix[i][j] - rightMatrix[i][j];
+                }
+            }
+
+            return mRes;
+        }
+
+        /// <summary>
+        /// Réalise la multiplication de deux matrices.
+        /// </summary>
+        /// <param name="leftMatrix">Matrice de gauche.</param>
+        /// <param name="rightMatrix">Matrice de droite</param>
+        /// <returns>Une matrice résultat carré correspondant à la multiplication des deux matrices d'entrée ou une matrice vide si les dimensions ne sont pas exactes.</returns>
+        public static int[][] Multiplication(int[][] leftMatrix, int[][] rightMatrix)
+        {
+            if (leftMatrix[0].Length != rightMatrix.Length)
+            {
+                return new int[0][];
+            }
+
+            int longueurCommune = rightMatrix.Length;
+            int mLineLength = leftMatrix.Length;
+            int mColLength = rightMatrix[0].Length;
+            int[][] mRes = new int[mLineLength][];
+
+            for (int i = 0; i < mLineLength; i++)
+            {
+                mRes[i] = new int[mColLength];
+                for (int j = 0; j < mColLength; j++)
+                {
+                    int resSum = 0;
+                    for (int k = 0; k < longueurCommune; k++)
+                    {
+                        resSum += leftMatrix[i][k] * rightMatrix[k][j];
+                    }
+                    mRes[i][j] = resSum;
+                }
+
+            }
+            return mRes;
+        }
+
+        // Exercice III - Crible d'Eratosthène
+
+        /// <summary>
+        /// Détermine la liste des n premiers nombres premiers d'après le crible d'Eratosthène.
+        /// </summary>
+        /// <param name="n"> Entier maximum.</param>
+        /// <returns>Une table d'entiers contenant tous les nombres premiers jusqu'à n.</returns>
+        public static int[] EratosthenesSieve(int n)
+        {
+            // Initialisation
+            bool[] premier = new bool[n + 1];
+            int compteurNombrePremiers = 0;
+
+            for (int i = 2; i < premier.Length; i++)
+            {
+                premier[i] = true;
+            }
+
+            int entier = 2;
+
+            // Traitement du crible d'Eratosthène
+            while (entier <= n) 
+            {
+                if (premier[entier]) 
+                {
+                    compteurNombrePremiers++;
+                    for (int j = entier * entier; j < n + 1; j += entier)  
+                    {
+                        premier[j] = false;
+                    }
+                }
+                entier++;
+            }
+
+            // Retranscription en tableau d'entiers
+            int[] tabRes = new int[compteurNombrePremiers];
+            compteurNombrePremiers = 0;
+
+            for (int i = 0; i < premier.Length; i++)
+            {
+                if (premier[i])
+                {
+                    tabRes[compteurNombrePremiers] = i;
+                    compteurNombrePremiers++;
+                }
+            }
+
+            return tabRes;
+
+        }
+
+        // Exercice IV - Questionnaire à choix multiple
+
+        // J'ai choisi cette structure car elle correspond aux données de l'énoncé.
+        /// <summary>
+        /// Contient une question d'un QCM avec les différentes réponses possibles, la solution et le poids de la question.
+        /// </summary>
+        public struct Qcm
+        {
+            public string Question;
+            public string[] Answers;
+            public int Solution;
+            public int Weight;
+        }
+
+        /// <summary>
+        /// Vérifie qu'une structure Qcm est valide, c'est-à-dire que la solution pointe bien sur une réponse et le poids est strictement positif.
+        /// </summary>
+        /// <param name="qcm">Structure Qcm à vérifier</param>
+        /// <returns>False si la structure n'est pas valide et True sinon.</returns>
+        public static bool QcmValidity(Qcm qcm)
+        {
+            if (qcm.Solution < 0 || qcm.Solution >= qcm.Answers.Length)
+            {
+                return false;
+            }
+
+            if (qcm.Weight <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int AskQuestion(Qcm qcm)
+        {
+            if (!QcmValidity(qcm))
+            {
+                throw new ArgumentException("Le qcm en entrée est invalide", "qcm");
+            }
+
+            Console.Write("\n" + qcm.Question + "\n");
+
+            for (int i = 0; i < qcm.Answers.Length; i++)
+            {
+                Console.Write($"{i + 1}. {qcm.Answers[i]} ");
+            }
+            Console.WriteLine();
+
+            string input;
+            int reponse = -1;
+
+            while (reponse < 0 || reponse >= qcm.Answers.Length)
+            {
+                Console.Write("Réponse : ");
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out reponse) || reponse < 0 || reponse >= qcm.Answers.Length )
+                {
+                    Console.WriteLine("\nRéponse invalide !\n");
+                }
+            }
+        }
+
+    }
+}
